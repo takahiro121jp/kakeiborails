11ad91ffd8a68f447b79c172eb6cf03823e01aa2
沖 貴大 <takahiro121jp@gmail.com>
2021-04-03 10:19:48 +0900

    20210403

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,24 @@
 class StaticsController < ApplicationController
   def setting
+    @setammounts = SetAmmount.new()
+  end
+
+  def create
+    @setammounts = Setammount.new(setting_params)
+    respond_to do |format|
+      if @setammount.save
+        format.html { redirect_to @setammount.notice:'Setammounts was successfully created.'}
+        format.json { render :show, status: :created, location: @book }
+      else
+      format.html { render :new }
+      format.json { render json: @setammount.errors, status: :unprocessable_entity }
+      end
 
   end
+
+  def setting_params
+    @setammounts = Setammount.new{setting_params}
+    params.require(:setting).permit(:title, :memo)
+  end
+end
 end
